c6be571e55fc0429d1b5f7a1a29424f6b4f5249f
Aviisaud <avisaud@yahoo.com>
2025-03-19 02:16:32 +0530

    Trust bagdes inserted

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -7,7 +7,7 @@ const HeroSection = () => {
       <div className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
         <div className="text-center md:text-left">
           <h1 className="text-4xl md:text-5xl font-bold mb-4 leading-tight">
-            Install & Update Printer Drivers with <br />
+            Install & Update Printer Drivers with
             All-in-One Printer Driver Updater
           </h1>
           <p className="text-lg mt-4 mb-8">
@@ -21,7 +21,30 @@ const HeroSection = () => {
           >
             Download Now
           </a>
+
+          {/* Trust badges with individual cards */}
+          <div className="mt-8 flex flex-wrap  gap-4">
+            {[
+              { src: "/img/AppEsteem_Seal_Logo.png", alt: "AppEsteem_Seal_Logo" },
+              { src: "/img/seal-digicert.png", alt: "DigiCert Seal" },
+              { src: "/img/trustpilot.jpg", alt: "TrustPilot" },
+            ].map((badge, index) => (
+              <div
+                key={index}
+                className="bg-white p-3 rounded-lg shadow-md flex items-center"
+              >
+                <Image
+                  src={badge.src}
+                  alt={badge.alt}
+                  width={100}
+                  height={60}
+                  className="h-15 object-contain"
+                />
+              </div>
+            ))}
+          </div>
         </div>
+
         <div className="flex justify-end">
           <Image
             src="/img/hero-image-new.webp"
